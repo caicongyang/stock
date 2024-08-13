8d5c97d76512ec2e3a8e5cd053e8dc05d7603882
caicongyang <caicongyang@gmail.com>
2024-08-13 16:07:45 +0800

    基于包名的读写分离

--- src/main/java/com/caicongyang/stock/controllers/IndexController.java
+++ src/main/java/com/caicongyang/stock/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package com.caicongyang.stock.controllers;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
+@Api(value = "index")
 public class IndexController {
 
     @GetMapping(value = "hello")
